9d2987c1f34ea48737cbeb37399a1be9e90f0694
Maddison <mtranchau1@gmail.com>
2022-12-11 18:46:47 -0800

    12/11/22: 6:46pm - created new screen to show saved locations on map

---
+++ app/src/main/java/com/example/cartracker/ShowSavedLocationsList.java
@@ -0,0 +1,16 @@
+package com.example.cartracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowSavedLocationsList extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_saved_locations_list);
+    } // end onCreate
+} // end ShowSavedLocationsList class
