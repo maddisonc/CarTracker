0ecefef192833e04cfcc0eb42c94b876e8f2c963
Maddison <mtranchau1@gmail.com>
2022-12-11 20:16:29 -0800

    12/11/22: 8:15pm - added code in MapsActivity to zoom in (animate camera) on last pin and toast to output the number of times a pin was clicked

--- app/src/main/java/com/example/cartracker/MapsActivity.java
+++ app/src/main/java/com/example/cartracker/MapsActivity.java
@@ -1,15 +1,18 @@
 package com.example.cartracker;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.cartracker.databinding.ActivityMapsBinding;
 
@@ -57,15 +60,42 @@ private ActivityMapsBinding binding;
         // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        // location that is zoomed in on
+        LatLng lastLocationPlaced = sydney;
         // for each location in savedLocations list
         for (Location location: savedLocations)
         {
+            // adds a marker based on location data
             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(latLng);
             markerOptions.title("Lat: " + location.getLatitude() + "Lon: " + location.getLongitude());
             mMap.addMarker(markerOptions);
-        }
+            lastLocationPlaced = latLng;
+        } // end foreach
 
-    }
-}
+        // zooms in on location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocationPlaced, 12f));
+
+        // click on a pin/marker
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener()
+        {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker)
+            {
+                // count the number of times pin is clicked
+                Integer clicks = (Integer) marker.getTag();
+                // tag is empty
+                if (clicks == null)
+                {
+                    clicks = 0;
+                }
+                clicks ++;
+                marker.setTag(clicks);
+                Toast.makeText(MapsActivity.this, "Marker " + marker.getTitle() +
+                        " was clicked " + marker.getTag() + " times.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }); // end marker listener
+    } // end map function
+} //end MapsActivity class
