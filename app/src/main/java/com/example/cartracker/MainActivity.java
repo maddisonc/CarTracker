46fdd9d5964b99032b424ec04b7bc56b8bd36781
Maddison <mtranchau1@gmail.com>
2022-12-11 17:24:44 -0800

    12/11/22: 5:23pm - began writing updateGPS function (intends to update location if permissions are granted) - currently only checks if version of Android is sufficient, calls requestPermissions method to check if permissions were granted

--- app/src/main/java/com/example/cartracker/MainActivity.java
+++ app/src/main/java/com/example/cartracker/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.cartracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity
 {
     public static final int DEFAULT_UPDATE_INTERVAL = 30;
     public static final int FASTEST_UPDATE_INTERVAL = 5;
+    // request code
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
 
     // dependency: Google Play Services - needed for maps, FusedLocation (added in gradle)
 
@@ -81,7 +92,52 @@ public class MainActivity extends AppCompatActivity
                 }
             } // end gps/cell/wifi switch onClick
         }); // end gps switch listener
+      } // end onCreate
 
-    } // end onCreate
+    // generated from main activity class, calls method when permissions are granted
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    updateGPS();
+                }
+                else
+                {
+                    Toast.makeText(this, "This app requires location permissions.", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void updateGPS()
+      {
+          // ask user for location permissions
+          //get current location from fused client
+          // update ui
+          fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+          if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+          {
+              // user gives permission
+              fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                  @Override
+                  public void onSuccess(Location location) {
+                      // store values of location and put into ui
+                  }
+              });
+          } // end if permissions given
+          else
+          {
+              // no permissions yet
+
+              // check if android build is recent enough (at least 23: M)
+              if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+              {
+                  requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+              }
+          } // end else no permissions
+      } // end updateGPS method
 
 } // end MainActivity
