7930e2bc556404655c35314fcacfdbd94096756a
Maddison <mtranchau1@gmail.com>
2022-12-11 16:47:41 -0800

    12/11/22: 4:46pm - set locationRequest properties (how often to update location (default and fastest intervals), priority level (how accurate))

--- app/src/main/java/com/example/cartracker/MainActivity.java
+++ app/src/main/java/com/example/cartracker/MainActivity.java
@@ -6,11 +6,28 @@ import android.os.Bundle;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+
 public class MainActivity extends AppCompatActivity {
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FASTEST_UPDATE_INTERVAL = 5;
+
+    // dependency: Google Play Services - needed for maps, FusedLocation (added in gradle)
 
     // instantiate ui elements
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
     Switch sw_locationupadtes, sw_gps;
+
+    // var that determines whether or not location is being tracked
+    boolean updateOn = false;
+
+    // LocationRequest - class that influences FusedLocationProvider settings
+    LocationRequest locationRequest;
+
+    // three sources of location data: gps, cell tower (triangulation used to find dist from each tower), wifi (connected to wifi)
+    // FusedLocationProvider: fuses all three sources - allows choice of very accurate data or less accurate data to save battery
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +47,15 @@ public class MainActivity extends AppCompatActivity {
         sw_locationupadtes = findViewById(R.id.sw_locationsupdates);
         sw_locationupadtes = findViewById(R.id.sw_gps);
 
-        // three sources of location data: gps, cell tower (triangulation used to find dist from each tower), wifi (connected to wifi)
-        // FusedLocationProvider: fuses all three sources - allows choice of very accurate data or less accurate data to save battery
+        // set LocationRequest properties
+        locationRequest = new LocationRequest();
 
+        // how often to update location (default val)
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+        // fastest - most frequent, less concerned with battery life
+        locationRequest.setFastestInterval(1000 * FASTEST_UPDATE_INTERVAL);
+        // accuracy level of location
+        locationRequest.setPriority(locationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
-        // dependency: Google Play Services - needed for maps, FusedLocation
     }
 }
