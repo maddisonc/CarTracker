8d9326d11aa30beb53974546302835f7cf453b85
Maddison <mtranchau1@gmail.com>
2022-12-11 16:26:31 -0800

    12/11/22: 4:25pm - ui elements linked in main activity java, app runs without errors (no logic yet)

--- app/src/main/java/com/example/cartracker/MainActivity.java
+++ app/src/main/java/com/example/cartracker/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.cartracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // instantiate ui elements
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+    Switch sw_locationupadtes, sw_gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // assign values to ui elements
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+
+        sw_locationupadtes = findViewById(R.id.sw_locationsupdates);
+        sw_locationupadtes = findViewById(R.id.sw_gps);
     }
 }
